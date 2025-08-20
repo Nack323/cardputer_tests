c6273b3d2e276fc0abacce81231b4604a0710026
Jose Palacios <jpalacios117@gmail.com>
2025-08-20 16:08:41 -0500

    fixed path

--- test1.py
+++ test1.py
@@ -7,12 +7,16 @@ info = {
         'todo' : [],
         'done' : []
         }
+def write_todo(obj):
+    with open('/sd/todo.json', 'w') as f:
+        json.dump(obj, indent = 1)
+
 try:
-    with open('todo.json', 'r') as f:
+    with open('/sd/todo.json', 'r') as f:
         info = json.load(f)
 except:
     # if file does not exist, write a new one
-    with open('todo.json', 'w') as f:
+    with open('/sd/todo.json', 'w') as f:
         json.dump(info, indent = 1)
 
 
