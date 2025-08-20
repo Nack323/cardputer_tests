d4b00a6216471438db7d71ca8cc7ceeaad3547a4
Jose Palacios <jpalacios117@gmail.com>
2025-08-20 15:58:12 -0500

    added mount sdcard

--- test1.py
+++ test1.py
@@ -1,2 +1,7 @@
-with open('sd/test1.txt', 'w') as f:
-    f.write('testing')
+from lib import sdcard
+
+SD = sdcard.SDCard()
+SD.mount()
+
+with open("/sd/test1.txt", "w") as f:
+    f.write("testing")
