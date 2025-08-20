111cc35e46cad97a168e48d9ebbe6cb729d59c77
Jose Palacios <jpalacios117@gmail.com>
2025-08-20 16:06:14 -0500

    added reader and writer for todo app

--- test1.py
+++ test1.py
@@ -1,7 +1,18 @@
 from lib import sdcard
+import json
 
 SD = sdcard.SDCard()
 SD.mount()
+info = {
+        'todo' : [],
+        'done' : []
+        }
+try:
+    with open('todo.json', 'r') as f:
+        info = json.load(f)
+except:
+    # if file does not exist, write a new one
+    with open('todo.json', 'w') as f:
+        json.dump(info, indent = 1)
 
-with open("/sd/test1.txt", "w") as f:
-    f.write("testingnow mounted")
+
